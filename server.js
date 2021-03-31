c71fefdc6cb44719db734fa314ab4328dc77ac66
NaderBhrr <nader.bhr.devv@gmail.com>
2021-03-31 21:16:19 +0430

    :sparkles: Create the initial server functionality

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+const fs = require('fs').promises;
+const path = require('path');
 const express = require('express');
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+app.get('/products', getProductsList);
 
-app.get('/products', (req, res) => {
-  res.send('Server working');
+async function getProductsList(req, res) {
+  const productsFile = path.join(__dirname, './products.json');
+
+  try {
+    const products = await fs.readFile(productsFile);
+    res.json(JSON.parse(products));
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+app.listen(PORT, () => {
+  console.log(
+    `The server started successfully on port: http://localhost:${PORT}`
+  );
 });
-
-app.listen(PORT, () =>
-  console.log(`Server started normally on port: http://localhost:${PORT}`)
-);
